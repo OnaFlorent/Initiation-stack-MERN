564cbff341f8077ffaf83666e17fd2c1bdcc0a86
OnaFlorent <bflorent53170@gmail.com>
2024-06-26 18:21:16 +0200

    Create update restaurant API and request

--- backend/src/routes/MyRestaurantRoute.ts
+++ backend/src/routes/MyRestaurantRoute.ts
@@ -15,7 +15,7 @@ const upload = multer({
 });
 
 // GET /api/my/restaurant
-router.get("/", jwtCheck, jwtParse, MyRestaurantController.getMyRestaurant)
+router.get("/", jwtCheck, jwtParse, MyRestaurantController.getMyRestaurant);
 
 // POST /api/my/restaurant
 router.post(
@@ -25,6 +25,15 @@ router.post(
   jwtCheck,
   jwtParse,
   MyRestaurantController.createMyRestaurant
+);
+
+router.put(
+  "/",
+  upload.single("imageFile"),
+  validateMyRestaurantRequest,
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.updateMyRestaurant
 );
 
 export default router;
