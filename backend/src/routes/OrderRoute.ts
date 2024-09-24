0c2e6a5e0565050c634aec4433af054760e064f8
OnaFlorent <bflorent53170@gmail.com>
2024-09-24 16:14:52 +0200

    Create session API

---
+++ backend/src/routes/OrderRoute.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import OrderController from "../controllers/OrderController";
+
+const router = express.Router();
+
+router.post(
+  "/checkout/create-checkout-sesssion",
+  jwtCheck,
+  jwtParse,
+  OrderController.createCheckoutSession
+);
+export default router;
